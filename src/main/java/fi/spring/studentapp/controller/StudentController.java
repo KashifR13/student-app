582937e9f77575d9a80a2fc55f8a4ee66355f696
Kashif Rasool <kashif.rasool@tuni.fi>
2023-02-09 18:52:32 +0200

    Develop Student REST controller

--- src/main/java/fi/spring/studentapp/controller/StudentController.java
+++ src/main/java/fi/spring/studentapp/controller/StudentController.java
@@ -1,4 +1,35 @@
 package fi.spring.studentapp.controller;
 
+import fi.spring.studentapp.model.Student;
+import fi.spring.studentapp.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@ResponseBody
+@RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
+
+    private final IStudentService studentService;
+
+    public StudentController(IStudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/save")
+    public String saveStudentData (@RequestBody Student student) {
+
+        studentService.saveStudent(student);
+        return "The data for new student is saved successfully.";
+    }
+
+    @GetMapping("/view")
+    public List<Student> getAllStudentsData() {
+
+        return studentService.getAllStudents();
+    }
 }
